ddae0fc44cc939ebcda65b3bfb83da6586965159
Haruki Hirai <iamprivateengineer@gmail.com>
2020-12-09 15:15:04 +0900

    PapaEventモデルテストのFactoryBot記述

---
+++ spec/factories/papa_events.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :papa_event do
+    started_at { Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all) }
+    do_what_id { 1 }
+    expected_reward { 5000 }
+    status_id { 1 }
+    user
+    papa
+  end
+end
